6e25dd7aee56bb65c6a049a7d58574f747b1461b
HarshaVardhan <athaluri.harsha@gmail.com>
2025-02-03 08:47:13 +0530

    associations

---
+++ associations/userservice/src/main/java/com/training/userservice/dao/User.java
@@ -0,0 +1,85 @@
+package com.training.userservice.dao;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+
+@Entity
+public class User {
+	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer uid;
+	private String name;
+	private String email;
+	
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "address_zip",referencedColumnName = "zip")
+	private Adress address;
+
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	public List<Hobbies> hobbies;
+	
+	@ManyToMany(cascade = CascadeType.ALL)
+	public List<Course> courses;
+	
+	public List<Hobbies> getHobbies() {
+		return hobbies;
+	}
+
+	public void setHobbies(List<Hobbies> hobbies) {
+		this.hobbies = hobbies;
+	}
+
+	public Adress getAddress() {
+		return address;
+	}
+
+	public void setAddress(Adress address) {
+		this.address = address;
+	}
+
+	public int getUid() {
+		return uid;
+	}
+
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+}
